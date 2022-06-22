f21b207cffaf803210a98355e387df603c6fe0b8
Rushabh Sakharkar <rushabh.sakharkar1998@gmail.com>
2022-06-22 16:58:44 +0530

    Again Commit from Main Branch

--- hello.js
+++ hello.js
@@ -6,4 +6,6 @@ console.log(arr);
 
 console.log("Byee");
 
-console.log("New Branch_1")
+console.log("New Branch_1");
+
+console.log("Test");
--- test.js
+++ test.js
@@ -5,3 +5,7 @@ console.log("Hello");
 console.log("ZZZ");
 
 console.log("For New Branch");
+
+var arr = [1,2,3,4,5,6,7,8,9];
+
+console.log(arr);
